6f922cd87ae70f04a467abde3cb9c0e628916eda
tyr4l <speadspawn255@web.de>
2015-05-20 13:39:30 +0200

    delete FOlder

--- Praktika/P5/A3/mailclient/routes/mails.js
+++ Praktika/P5/A3/mailclient/routes/mails.js
@@ -21,4 +21,21 @@ router.route('/mails/:id').get(function(req, res){
 	});
 });
 
+router.route('/mails/:id').delete(function(req, res){
+	Mails.remove({
+		folder: req.params.id
+	},function(err, mails){
+		if(err){
+			return res.send(err);
+		}
+		res.json({message:"Successfully deleted"});
+	});
+});
+
+router.route('/mails/:id').put(function(req, res){
+	console.log(req.params.id);
+	console.log(req.body.name);
+})
+
+
 module.exports = router;
