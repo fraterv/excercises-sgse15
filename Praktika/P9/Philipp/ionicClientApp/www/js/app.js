af19d3d708b14a76a6256e3e3d74ad382a403fda
Philipp Kroos <philipp.kroos@fh-bielefeld.de>
2015-06-12 11:39:55 +0200

    Client showing folders from mongo.

--- Praktika/P9/Philipp/ionicClientApp/www/js/app.js
+++ Praktika/P9/Philipp/ionicClientApp/www/js/app.js
@@ -47,12 +47,12 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
-    .state('app.playlists', {
-      url: "/playlists",
+    .state('app.folders', {
+      url: "/folders",
       views: {
         'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
+          templateUrl: "templates/folders.html",
+          controller: 'FoldersCtrl'
         }
       }
     })
@@ -67,5 +67,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/folders');
 });
---
+++ Praktika/P9/Philipp/ionicClientApp/www/js/services.js
@@ -0,0 +1,17 @@
+angular.module('starter.services', []).factory('Folders', ['$http', function($http) {
+    return {
+        getAll:function() {
+            return $http.get('http://localhost:3000/api/mails');
+        },
+        get:function(id){
+            return $http.get('http://localhost:3000/api/mails/'+id);
+        },
+        edit:function(id,newname){
+            var query = id + "?folder=" + newname;
+            return $http.put('http://localhost:3000/api/mails/'+query);
+        },
+        delete:function(id){
+            return $http.delete('http://localhost:3000/api/mails/' + id);
+        }
+    }
+}]);
